fad885e2344121067bf221a976d244126ee625bb
Kelly Farrell <kellbot@gmail.com>
2011-01-28 18:07:03 -0500

    Added missing R3 files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Brainstormer::Application.config.secret_token = 'ef766f0bd2e4fc807e2d747bb20d69259a6298c795752a0f7fb2497c16872a3e1b78a8dbdb7e90439c061f92dbd76b91ed8d0f946f0f6488c3b5377d3ba804f4'
