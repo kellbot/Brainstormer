0edf67039f9c0d5ee392e4de6edcdfb4801e689e
Kelly Farrell <kellbot@gmail.com>
2011-02-03 16:42:44 -0500

    Argh

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
 
   def authenticate
     authenticate_or_request_with_http_basic do |username, password|
-      username == "skillslate" && password == "hirepeeps"
+      username == "secret" && password == "goaway"
     end
   end
 end
